e175520f33d335ec784d23627b9d60c95b1afa3a
PC09 <pc09@fh-westkueste.de>
2025-03-20 12:46:31 +0100

    Pausenalarm implementiert

--- playground/coffeetime/main.py
+++ playground/coffeetime/main.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
-print("Willkommen zur Kaffee-Erinnerungs-App!")
 
-print("TRINK!")
+import time
+import winsound
+from datetime import datetime
+
+if __name__ == "__main__":
+    
+    print("Willkommen zur Kaffee-Erinnerungs-App!")
+    middach = False
+
+    while True:
+
+        # Hole die aktuelle Uhrzeit
+        current_time = datetime.now().strftime("%H:%M").split(":")
+        
+        if len(current_time) == 2:
+
+            # Prüfe, ob es 12:00 Uhr ist
+            if current_time[0] == "12" and int(current_time[1]) == 0 and middach == False:
+                middach = True
+                print("Middach!")
+                winsound.Beep(2500, 1000) # Beep mit 2500Hz für 1 
+            else:
+                middach = False
+                
+        else:
+            raise ValueError("Ungültige Uhrzeit")
+
+        time.sleep(40)  # Für 40 Sekunden warten
 
 exit(0)
