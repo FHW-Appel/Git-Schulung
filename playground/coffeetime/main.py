598c79eae106134a6f741152c70606c97021e3e1
zilfhw <zilske@fh-westkueste.de>
2025-03-20 11:04:21 +0100

    jetzt aber

--- playground/coffeetime/main.py
+++ playground/coffeetime/main.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python3
-
-import time
-
-def main():
-    sip_number = 1
-    while True:
-        time.sleep(120)  # Warte 2 Minuten
-        print(f"TRINK! Schluck Nummer: {sip_number}")
-        sip_number += 1
-
-if __name__ == "__main__":
-    main()
-
+print("TRINK!")
 exit(0)
